6bc313161c926652db1cfcb226dc81af55d84467
DimaAlabsi <dema.yaser8@gmail.com>
2021-09-14 16:12:14 +0300

    lab07 has been finished

--- server.js
+++ server.js
@@ -5,18 +5,22 @@ require("dotenv").config();
 const cors = require("cors");
 const app = express();
 app.use(cors());
-const weatherData = require("./data/Weather.json");
+const weatherData = require('./data/Weather.json');
 const PORT = process.env.PORT;
 
-app.get('/data',(req,res)=>{
-  res.status(200).send('Hi from Jordan');
 
+app.listen(PORT, () => {});
+
+app.get("/data", (req, res) => {
+  res.status(200).send("Hi from Jordan");
 });
 
 class Forecast {
   constructor(date, description) {
     (this.date = date), (this.description = description);
   }
+
+  // searchQuery
 }
 
 app.get("/weather", (req, res) => {
@@ -25,15 +29,19 @@ app.get("/weather", (req, res) => {
   let city_name = req.query.searchQuery.toLowerCase();
 
   if (city_name) {
-    const arr = weatherData.find((item) => {
+    let arr = weatherData.find((item) => {
       return item.city_name.toLowerCase() === city_name;
     });
-    let weatherDetails = arr.data.map((i) => {
-      return new Forecast(i.datetime, i.weather.description);
-    });
+    if (arr) {
+      let weatherDetails = arr.data.map((i) => {
+        return new Forecast(i.datetime, i.weather.description);
+      });
 
-    res.json(weatherData);
+      res.status(200).json(weatherDetails);
+    } else {
+      res.status(404).send("This location doesnot found");
+    }
+  } else {
+    res.status(400).send("Something went wrong");
   }
-});
-app.listen(PORT, () => {
 });
